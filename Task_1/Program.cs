76e9592f163c447005ac7692914442b356bb78a1
Иван <polushinv@gmail.com>
2022-07-14 16:50:51 +0300

    ДЗ-9 готово

---
+++ Task_1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int Promt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(System.Console.ReadLine());
+}
+
+void PrintNaural(int numberM, int numberN)
+{
+    if (numberM > numberN)
+    {
+        return;
+    }
+    if (numberN % 2 == 1)
+    {
+        numberN--;
+    }
+    PrintNaural(numberM, numberN - 2);
+    System.Console.Write($"{numberN}; ");
+}
+
+int numberM = Promt("Введите число M: ");
+int numberN = Promt("Введите число N: ");
+PrintNaural(numberM, numberN);
+System.Console.WriteLine();
+
+
---
+++ Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int Promt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(System.Console.ReadLine());
+}
+
+int PrintSumNumbers(int numberM, int numberN)
+{
+    if (numberM > numberN)
+    {
+        return 0;
+    }
+    return PrintSumNumbers(numberM, numberN - 1) + numberN;
+}
+
+int numberM = Promt("Введите число M: ");
+int numberN = Promt("Введите число N: ");
+int answer = PrintSumNumbers(numberM, numberN);
+System.Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N равна: {answer}");
+
+
+
